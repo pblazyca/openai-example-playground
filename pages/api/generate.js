72eb02bc4e6baf70a5ab8f88e622b360e29a9a36
pblazyca <pblazyca@outlook.com>
2023-03-02 20:25:43 +0100

    Updated to chat-gpt model.

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -28,13 +28,13 @@ export default async function (req, res) {
   }
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
       max_tokens: 256,
-      prompt: generatePrompt(prompt),
+      messages: generatePrompt(prompt),
       temperature: parseFloat(temp)
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
+    res.status(200).json({ result: completion.data.choices[0].message.content });
   } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
@@ -52,5 +52,5 @@ export default async function (req, res) {
 }
 
 function generatePrompt(prompt) {
-  return `Suggest five names for a C# project described by ${prompt}.`;
+  return [{ "role": "user", "content": `${prompt}` }];
 }
